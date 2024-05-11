58b0e5b41468880e2f50a2b967e54d6d1ddbeb60
vanessa <vanessa.bert311@gmail.com>
2024-05-11 20:44:28 -0300

    service user implements

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -4,6 +4,10 @@ import { CardModule } from 'primeng/card';
 import { UserService } from '../../services/user/user.service';
 import { CookieService } from 'ngx-cookie-service';
 import { MessageService } from 'primeng/api';
+import { AuthRequest } from '../../models/interfaces/user/auth/AuthRequest';
+import { response } from 'express';
+import { error } from 'console';
+import { SignupUserRequest } from '../../models/interfaces/user/SignupUserRequest';
 
 @Component({
   selector: 'app-home',
@@ -32,11 +36,62 @@ export class HomeComponent {
   ) {}
 
   onSubmitLoginForm() : void {
+    if(this.loginForm.value && this.loginForm.valid){
+      this.userService.authUser(this.loginForm.value as AuthRequest).subscribe({
+        next: (response) => {
+          if(response){
+            this.cookieService.set('USER_INFO', response?.token);
+            this.loginForm.reset();
 
+            this.messageService.add({
+              severity: 'sucess',
+              summary: 'sucesso',
+              detail: `Bem vindo de volta ${response?.name}`,
+              life: 2000
+            },
+          )}
+        },
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: `Erro ao fazer login`,
+            life: 2000
+          });
+          console.log(err);
+        }
+      })
+    }
   }
 
   onSubmitSignupForm() : void {
-
+    if(this.signupForm.value && this.signupForm.valid){
+      this.userService
+        .signupUser(this.signupForm.value as SignupUserRequest)
+        .subscribe({
+          next: (response) => {
+            if(response){
+              this.signupForm.reset();
+              this.loginCard = true;
+              this.messageService.add({
+                severity: 'sucess',
+                summary: 'sucesso',
+                detail: `Bem vindo de volta ${response?.name}`,
+                life: 2000
+              })
+            }
+          },
+          error: (err) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: `Erro ao fazer login`,
+              life: 2000
+            })
+            console.log(err);
+          }
+        })
+    }
   }
 
 }
